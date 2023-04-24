d12ff85f90d18df6dc78cf2593307407c156f06a
byiringiroscar <byiringoroscar@gmail.com>
2023-04-24 23:32:05 +0200

    Add desing on card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import './main.css';
 import Card from './Card';
+import loading from '../images/loading.gif';
 
 const Main = () => {
   const { games, isLoading } = useSelector((state) => state.games);
   if (isLoading) {
     return (
       <main>
-        <h2>Loading .....</h2>
+        <img className="loading-screen" src={loading} alt="loading" />
       </main>
     );
   }
   return (
     <main>
       <div className="card-container">
-        {games.map((item) => (<Card key={item.id} />))}
+        {games.map((item) => (<Card key={item.id} title={item.title} image={item.thumbnail} />))}
       </div>
     </main>
   );
